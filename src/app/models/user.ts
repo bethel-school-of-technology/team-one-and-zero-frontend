e40801149be8b1f5a3dc148266fb3a76929f6170
Daniel Gilman <143534817+17oogway04@users.noreply.github.com>
2024-05-13 13:46:10 -0500

    Update user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,17 +1,17 @@
 export class User {
-    UserId?: string; 
-    Username?: string;
-    Password?: string;
-    Email?: string; //is there a way for this to have a different type, such as EmailAddress like it does in the backend?
-    Description?: string;
-    Birthdate?: string; 
+    userId?: string; 
+    username?: string;
+    password?: string;
+    email?: string; //is there a way for this to have a different type, such as EmailAddress like it does in the backend?
+    description?: string;
+    birthdate?: string; 
 
-    constructor(UserId?: string,  Username?: string, Password?: string, Email?: string, Description?: string, Birthdate?: string){
-        this.UserId = UserId;
-        this.Username = Username;
-        this.Password = Password;
-        this.Email = Email;
-        this.Description = Description;
-        this.Birthdate = Birthdate;
+    constructor(userId?: string,  username?: string, password?: string, email?: string, description?: string, birthdate?: string){
+        this.userId = UserId;
+        this.username = Username;
+        this.password = Password;
+        this.email = Email;
+        this.description = Description;
+        this.birthdate = Birthdate;
     }
 }
