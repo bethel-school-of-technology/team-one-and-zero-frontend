42b6d3b1a72154d5ca2c1955d597e802cd8eac7d
Penelope Wedemeyer <pennygracew@gmail.com>
2024-05-13 10:05:26 -0700

    Imorted the HttpClientModule. Set up the service with the CRUD methods

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comment } from '../models/comment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  baseURL: string = "http://localhost:5167/api/comment";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllComments(): Observable<Comment[]> {
+    return this.http.get<Comment[]>(this.baseURL);
+  }
+
+  getComment(commentId: number): Observable<Comment> {
+    return this.http.get<Comment>(`${this.baseURL}/${commentId}`)
+  }
+
+  createComment(newComment: Comment): Observable<Comment> {
+    return this.http.post<Comment>(this.baseURL, newComment);
+  }
+
+  updateComment(newComment: Comment): Observable<Comment> {
+    return this.http.put<Comment>(`${this.baseURL}/${newComment.commentId}`, newComment);
+  }
+
+  deleteComment(commentId: number): Observable<any> {
+    return this.http.delete<any>(`${this.baseURL}/${commentId}`);
+  }
 }
