96faf94d740f92a1c9b879b7d6279cbf976ecfdd
Penelope Wedemeyer <113943772+PenelopeWedemeyer@users.noreply.github.com>
2024-05-13 11:50:00 -0700

    Update user.ts
    Fixed capitalization and got rid of the comment

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,16 +2,16 @@ export class User {
     userId?: string; 
     username?: string;
     password?: string;
-    email?: string; //is there a way for this to have a different type, such as EmailAddress like it does in the backend?
+    email?: string; 
     description?: string;
     birthdate?: string; 
 
     constructor(userId?: string,  username?: string, password?: string, email?: string, description?: string, birthdate?: string){
-        this.userId = UserId;
-        this.username = Username;
-        this.password = Password;
-        this.email = Email;
-        this.description = Description;
-        this.birthdate = Birthdate;
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.description = description;
+        this.birthdate = birthdate;
     }
 }
