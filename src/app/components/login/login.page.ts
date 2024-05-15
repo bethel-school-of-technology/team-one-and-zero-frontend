ed1092c702502c939e4eed4dc9e8e089b22eeae5
Penelope Wedemeyer <pennygracew@gmail.com>
2024-05-15 14:16:50 -0700

    made the form and signup link functional.

--- src/app/components/login/login.page.ts
+++ src/app/components/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+
+  constructor(private myUserService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.myUserService.login(this.username, this.password).subscribe((response:any) => {
+      window.alert("User Logged in Successfully"); // May change this to a prompt
+      this.router.navigate(['home']);
+    }, error => {
+      console.log('Error: ', error),
+      window.alert('Unsuccessful Login'); // May change this to a prompt
+    })
   }
 
 }
