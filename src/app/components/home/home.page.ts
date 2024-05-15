4860e654e677d02c68d9eb5d5af947a343c032d5
TaysonTrihey <tdtrihey@gmail.com>
2024-05-14 19:48:01 -0600

    adding external api call

--- src/app/components/home/home.page.ts
+++ src/app/components/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  constructor(private api : ApiService) {
+    this.api.getSongs().subscribe(data=>{
+      console.log(data)
+    }
+  )}
 }
