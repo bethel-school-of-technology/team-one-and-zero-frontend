205fa7ad8a0469bb086fa872cc93444ae395c990
Penelope Wedemeyer <pennygracew@gmail.com>
2024-05-29 07:58:12 -0700

    My git is acting funny so I need to push this and try again

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,12 +4,16 @@ export class User {
     password?: string;
     email?: string; 
     description?: string;
+    birthdate?: string; 
+    profilePic?: string;
     
-    constructor( userId?: string, username?: string, password?: string, email?: string, description?: string){
+    constructor( userId?: string, username?: string, password?: string, email?: string, description?: string, birthdate?: string, profilePic?: string){
         this.userId = userId;
         this.username = username;
         this.password = password;
         this.email = email;
         this.description = description;
+        this.birthdate = birthdate;
+        this.profilePic = profilePic;
     }
 }
