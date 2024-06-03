bccd74f728bcf070f356f438a2661234ededac87
Penelope Wedemeyer <pennygracew@gmail.com>
2024-06-03 12:35:45 -0700

    Added songId property to comment model

--- src/app/models/comment.ts
+++ src/app/models/comment.ts
@@ -2,12 +2,14 @@ export class Comment {
     commentID?: string;
     username?: string;  // may need to be changed in the future?
     description?: string;
+    songId?: string;
     postedAt?: string; // variable type may need to be changed (bc API has the post as a DatTime. Depends if it works or not)
 
-    constructor(commentID?: string, username?:string, description?: string, postedAt?: string) {
+    constructor(commentID?: string, username?:string, description?: string, songId?: string, postedAt?: string) {
         this.commentID = commentID;
         this.username = username;
         this.description = description;
+        this.songId = songId;
         this.postedAt = postedAt;
     }
 }
